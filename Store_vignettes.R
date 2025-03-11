e6ea5a21b9e321267763227646e1d8f8899f445d
Ed Carnell <edward.j.carnell@gmail.com>
2025-03-11 18:43:03 +0000

    Add SpatialUKCEH

--- Store_vignettes.R
+++ Store_vignettes.R
@@ -2,22 +2,28 @@ require(magrittr)
 require(devtools)
 require(xml2)
 require(SpatialUKCEH)
-create_dir("./html/")
 
 setwd("~/ECarnell.github.io/")
+create_dir("./html/")
+
+
 
 # Create a new HTML document
-index_html <- read_html("<html><head><title>Vignettes</title></head><body></body></html>")
+index_html <- read_html("./index_template.html")
 
 # Add title and subheading
 body_node <- xml_find_first(index_html, "//body")
-title_node <- xml_add_child(body_node, "h1", "Vignettes")
-subheading_node <- xml_add_child(body_node, "h2", "Explore the vignettes below:")
 
+build_vig <- F
+if(build_vig){
 devtools::install_github("ECarnell/AgricInvUKCEH",
                          ref = "main",
                          build_vignettes = T,
                          auth_token = Sys.getenv("GITHUBTOKEN"))
+  devtools::install_github("ECarnell/SpatialUKCEH",
+                           ref = "main",
+                           build_vignettes = T,
+                           auth_token = Sys.getenv("GITHUBTOKEN"))}
 
 
 lib_pth <- .libPaths() %>% .[grepl("AppData",.)]
@@ -27,8 +33,7 @@ vgn <- paste0(lib_pth,"/AgricInvUKCEH/doc/") %>% list.files(pattern = ".html",re
 
 vgn <- vgn[!grepl("index",vgn)]
 
-
-
+subheading_node <- xml_add_child(body_node, "h2", "AgricInvUKCEH package:")
 
 for(v in vgn){
 print(v)
@@ -43,5 +48,28 @@ new_link <- paste0('<a href="html/',bse,'">',nme,'</a>')
 link_node <- xml_add_child(body_node, "a", nme, href = paste0("html/", bse))
 xml_add_child(body_node, "br")
 
+}
+
+
+vgn <- paste0(lib_pth,"/SpatialUKCEH/doc/") %>% list.files(pattern = ".html",recursive = T,
+                                                            full.names = T)
+
+vgn <- vgn[!grepl("index",vgn)]
+
+subheading_node <- xml_add_child(body_node, "h2", "AgricInvUKCEH package:")
+
+for(v in vgn){
+  print(v)
+  bse <- basename(v)
+  nme <- bse %>% gsub("_"," ",.) %>% gsub("\\.html","",.)
+  nw_fl <- paste0("./html/",bse)
+  
+  file.copy(from = v,
+            to = nw_fl)
+  
+  new_link <- paste0('<a href="html/',bse,'">',nme,'</a>')
+  link_node <- xml_add_child(body_node, "a", nme, href = paste0("html/", bse))
+  xml_add_child(body_node, "br")
+  
 }
 write_html(index_html, "./index.html")
